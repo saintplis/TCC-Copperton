d1aa8c5593868b3a73821f5f708998a640dbb169
Rafael Dos Santos <101225450+saintplis@users.noreply.github.com>
2023-11-15 19:08:17 -0300

    Delete Desenvolvimento directory

--- Desenvolvimento/Cadastro-Funcionario/code/validation.js
+++
@@ -1,6 +0,0 @@
-$('#senha, #confirmar').on('keyup', function () {
-    if ($('#senha').val() == $('#confirmar').val()) {
-      $('#password-validation').html('As senhas combinam').css('color', 'green');
-    } else 
-      $('#password-validation').html('As senhas não combinam').css('color', 'red');
-  });
--- Desenvolvimento/Cadastro-Produto/code/protect.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-if(!isset($_SESSION)){
-    session_start();
-}
-if(!isset($_SESSION['admin'])) {
-    die(header("Location: http://localhost/desenvolvimento/Login/code/index.php"));
-}
-?>
